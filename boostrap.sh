d560a8c308a8ce32843e14eb8d5d63d5703948aa
Marcos Rocha <mmurilo@pihole-6.home.vitoriorocha.net>
2025-08-03 21:37:53 +0000

    add base folders

---
+++ boostrap.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu
+
+stacks_path=/opt/stacks
+
+
+echo "Update System"
+apt update
+apt full-upgrade -y
+
+echo "##########   install extra packages ###########"
+apt install curl git neovim
+
+echo "##########   install docker   #################"
+curl -fsSL https://get.docker.com | sh
+
+echo "#######################################################################"
+echo "##################   prepare path #####################################"
+mkdir -p $stacks_path
+cd $stacks_path
+
+echo "#######################################################################"
+echo "##################   clone repo   #####################################"
+#git clone 
+
